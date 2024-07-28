036b1531e98c24a15a56b415d5247874f2f29613
zluckyH <zluckyhou@users.noreply.github.com>
2024-07-28 19:06:27 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import os
+import mimetypes
+import shutil
+import re
+
+
+
+uploaded_file = st.file_uploader("Upload File")
+
+
+st.markdown(f"streamlit file type: {uploaded_file.type}")
+
+mime_type, _ = mimetypes.guess_type(uploaded_file.name)
+
+st.markdown(f"mimetypes: {mime_type}")
