d31e03f98bb91a329e936d4f5b59ca1b793f8eb8
zluckyH <zluckyhou@users.noreply.github.com>
2024-07-28 19:08:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 import streamlit as st
 import os
 import mimetypes
@@ -8,9 +10,9 @@ import re
 
 uploaded_file = st.file_uploader("Upload File")
 
+if uploaded_file:
+	st.markdown(f"streamlit file type: {uploaded_file.type}")
 
-st.markdown(f"streamlit file type: {uploaded_file.type}")
+	mime_type, _ = mimetypes.guess_type(uploaded_file.name)
 
-mime_type, _ = mimetypes.guess_type(uploaded_file.name)
-
-st.markdown(f"mimetypes: {mime_type}")
+	st.markdown(f"mimetypes: {mime_type}")
